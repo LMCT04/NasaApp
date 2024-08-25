f28729e4b6d6cc11aed495d6108c5b4c2c1974d8
LMCT04 <100221599+LMCT04@users.noreply.github.com>
2024-08-25 15:37:34 -0300

    connect apiRest and utils

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,7 +1,26 @@
 import { StyleSheet, Text, View } from "react-native";
 import Header from "../../components/Header";
+import fetchApi from "../../utils/fetch";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [todaysImage, setTodaysImage] = useState();
+
+  useEffect(() => {
+    const loadTodaysImage = async () => {
+      try {
+        const todaysImageResponse = await fetchApi();
+        setTodaysImage(todaysImageResponse);
+      } catch (error) {
+        console.error(error);
+        setTodaysImage(undefined);
+      }
+    };
+    loadTodaysImage().catch(null);
+  }, []);
+
+  console.log(todaysImage);
+
   return (
     <View style={styles.container}>
       <Header />
